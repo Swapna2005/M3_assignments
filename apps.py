ec6ed248345e77377f0fadba244b4878ca498b03
Swapna2005 <146418921+Swapna2005@users.noreply.github.com>
2023-11-25 16:46:44 +0530

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PythonappConfig(AppConfig):
+class JavaappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pythonapp'
+    name = 'javaapp'
