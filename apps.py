510a718d3bd39bacee701eebe73cfc7b44a1fdf1
Swapna2005 <146418921+Swapna2005@users.noreply.github.com>
2023-11-25 16:48:18 +0530

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class JavaappConfig(AppConfig):
+class HtmlappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'javaapp'
+    name = 'htmlapp'
--- models.py
+++ models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    age = models.IntegerField()
+    place = models.CharField(max_length=100)
